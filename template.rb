c9702ff44ba64295abc47aa43f960f3e0a59a8ae
Mason Roberts <mrobe18@gmail.com>
2025-02-12 23:10:16 -0500

    Update template.rb
    tailwind config

--- template.rb
+++ template.rb
@@ -1,7 +1,8 @@
 require 'fileutils'
 
-# Modify gitignore and workflow for rails and node for basic setup.
+# Modify gitignore and github workflow, add basic tailwind config.
 # The added weight of the gem files should not be pushed up to github.
+# Fix workflow so initial run does not fail due to ignored folder.
 
 # When cloning the repo, run bundle or bundle install and Rails will review the gem / 
 # gem-lock file and install the missing gems. Pushing that weight is not needed.
@@ -11,7 +12,7 @@ require 'fileutils'
 # and working. Uses PostgreSQL in workflow. 
 
 # NOTE - This will completely rewrite these files.
-# NOTW - Default used with modifications noted.
+# NOTE - Default files used with modifications noted.
 
 # Method to write a file, deleting existing file if necessary
 def write_file(file_path, content)
@@ -141,11 +142,12 @@ jobs:
       - name: Install packages
         run: sudo apt-get update && sudo apt-get install --no-install-recommends -y google-chrome-stable curl libjemalloc2 libvips postgresql-client
 
-      - name: Checkout code # Grab the code for the package json
+      - name: Checkout code # Grab the code [ package json, gem file, good stuff ]
         uses: actions/checkout@v4
 
       - name: Set up Ruby # Will run bundle auto on setup I assume because bundler-cache true.
-        uses: ruby/setup-ruby@v1
+        uses: ruby/setup-ruby@v1Gifs
+
         with:
           ruby-version: .ruby-version
           bundler-cache: true
@@ -168,3 +170,40 @@ jobs:
 YAML
   )
 end
+
+
+# Ensure the config directory exists
+config_dir = 'config'
+FileUtils.mkdir_p(config_dir) unless Dir.exist?(config_dir)
+
+# Create the tailwind.config file
+inside(workflow_dir) do
+  write_file('tailwind.config', <<-CODE
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+    
+  ]
+}
+
+CODE
+)
