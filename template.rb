33a4418b758de7bf4225e4ca404ef6375718b0a0
Mason Roberts <mrobe18@gmail.com>
2025-02-14 09:06:13 -0500

    Update template.rb
    Create controller, partial, and css classes. Update methods for better functionality. Include error handling and logging.

--- template.rb
+++ template.rb
@@ -1,170 +1,133 @@
 require 'fileutils'
 
-# Modify gitignore and workflow for rails and node for basic setup.
-# The added weight of the gem files should not be pushed up to github.
+# Copy paste this erb line into views/layouts/application.html.erb where you want alerts:
+# <%= render "shared/flash_notice" %>  
 
-# When cloning the repo, run bundle or bundle install and Rails will review the gem / 
-# gem-lock file and install the missing gems. Pushing that weight is not needed.
+# Create stimulus controller for flash messages - alert / notice
+# Create flash partial and shared directory, if dir not present
+# Append classes to application.css file
 
-# This templates will add the lines to the gitignore, cutting the weight of the tracked 
-# files, and modify the workflow job - test to ensure the container is set up properly
-# and working. Uses PostgreSQL in workflow. 
+# NOTE - This will completely rewrite flash_controller in javascript controllers.
+# NOTE - This will create a shared folder in views if none present.
+# NOTE - This will create a flash partial in shared folder.
+# NOTE - Classes will be appended to application.css file.
 
-# NOTE - This will completely rewrite these files.
-# NOTW - Default used with modifications noted.
-
-# Method to write a file, deleting existing file if necessary
+# Method to write a file, overwriting existing content
 def write_file(file_path, content)
-  if File.exist?(file_path)
-    File.delete(file_path)  # Delete the existing file
-  end
   File.open(file_path, 'w') do |file|
-    file.write(content)  # Write the new content
+    file.write(content)
   end
+rescue => e
+  puts "Error writing to #{file_path}: #{e.message}"
 end
 
-# Add lines to .gitignore
-write_file('.gitignore', <<-CODE
-# ignore the gems of bundle
-/vendor/bundle
+# Method to append content to a file if it doesn't already exist
+def append_to_file(file_path, content)
+  existing_content = File.read(file_path) if File.exist?(file_path)
+  unless existing_content&.include?(content)
+    File.open(file_path, 'a') do |file|
+      file.puts(content)
+    end
+    puts "Appended CSS classes to #{file_path}."
+  else
+    puts "CSS classes already exist in #{file_path}. Skipping."
+  end
+rescue => e
+  puts "Error appending to #{file_path}: #{e.message}"
+end
 
-# Ignore bundler config.
-/.bundle
+# Ensure the stimulus javascript directory exists
+javascript_dir = 'app/javascript/controllers'
+FileUtils.mkdir_p(javascript_dir) unless Dir.exist?(javascript_dir)
+puts "Created directory: #{javascript_dir}"
 
-# Ignore all environment files.
-/.env*
+# Create stimulus flash controller file
+inside(javascript_dir) do
+  write_file('flash_controller.js', <<-CODE
+  import { Controller } from "@hotwired/stimulus"
 
-# Ignore all logfiles and tempfiles.
-/log/*
-/tmp/*
-!/log/.keep
-!/tmp/.keep
-
-# Ignore pidfiles, but keep the directory.
-/tmp/pids/*
-!/tmp/pids/
-!/tmp/pids/.keep
-
-# Ignore storage (uploaded files in development and any SQLite databases).
-/storage/*
-!/storage/.keep
-/tmp/storage/*
-!/tmp/storage/
-!/tmp/storage/.keep
-
-/public/assets
-
-# Ignore master key for decrypting credentials and more.
-/config/master.key
-
-/app/assets/builds/*
-!/app/assets/builds/.keep
+  // Flash controller for alerts and notices of app
+  // Will fade out flash messages after 3 seconds
+  // Prevents messages from hanging out until pages is refreshed.
+  export default class extends Controller {
+    connect() {
+      setTimeout(() => {
+        this.hide();
+      }, 3000); // 3000 milliseconds = 3 seconds
+    }
+  
+    hide() {
+      this.element.style.transition = "opacity 1s";
+      this.element.style.opacity = "0";
+      setTimeout(() => {
+        this.element.remove();
+      }, 1000); // Wait for the transition to finish before removing the element
+    }
+  
+    remove(event) {
+      if (event.animationName === "fadeOut") {
+        this.element.remove();
+      }
+    }
+  }
 CODE
-)
-
-# Ensure the workflows directory exists
-workflow_dir = '.github/workflows'
-FileUtils.mkdir_p(workflow_dir) unless Dir.exist?(workflow_dir)
-
-# Modify the ci.yml file
-inside(workflow_dir) do
-  write_file('ci.yml', <<-YAML
-name: CI
-
-on:
-  pull_request:
-  push:
-    branches: [ main ]
-
-jobs:
-  scan_ruby:
-    runs-on: ubuntu-latest
-
-    steps:
-      - name: Checkout code
-        uses: actions/checkout@v4
-
-      - name: Set up Ruby
-        uses: ruby/setup-ruby@v1
-        with:
-          ruby-version: .ruby-version
-          bundler-cache: true
-
-      - name: Scan for common Rails security vulnerabilities using static analysis
-        run: bin/brakeman --no-pager
-
-  scan_js:
-    runs-on: ubuntu-latest
-
-    steps:
-      - name: Checkout code
-        uses: actions/checkout@v4
-
-      - name: Set up Ruby
-        uses: ruby/setup-ruby@v1
-        with:
-          ruby-version: .ruby-version
-          bundler-cache: true
-
-      - name: Scan for security vulnerabilities in JavaScript dependencies
-        run: bin/importmap audit
-
-  lint:
-    runs-on: ubuntu-latest
-    steps:
-      - name: Checkout code
-        uses: actions/checkout@v4
-
-      - name: Set up Ruby
-        uses: ruby/setup-ruby@v1
-        with:
-          ruby-version: .ruby-version
-          bundler-cache: true
-
-      - name: Lint code for consistent style
-        run: bin/rubocop -f github
-
-  test:
-    runs-on: ubuntu-latest
-
-    services:
-      postgres:
-        image: postgres
-        env:
-          POSTGRES_USER: postgres # Default for postgres. Container will be destroyed after run.
-          POSTGRES_PASSWORD: postgres # Default for postgres.
-        ports:
-          - 5432:5432
-        options: --health-cmd="pg_isready" --health-interval=10s --health-timeout=5s --health-retries=3
-
-    steps:
-      - name: Install packages
-        run: sudo apt-get update && sudo apt-get install --no-install-recommends -y google-chrome-stable curl libjemalloc2 libvips postgresql-client
-
-      - name: Checkout code # Grab the code for the package json
-        uses: actions/checkout@v4
-
-      - name: Set up Ruby # Will run bundle auto on setup I assume because bundler-cache true.
-        uses: ruby/setup-ruby@v1
-        with:
-          ruby-version: .ruby-version
-          bundler-cache: true
-
-      - name: Run tests # Run Rails app tests
-        env:
-          RAILS_ENV: test
-          DATABASE_URL: postgres://postgres:postgres@localhost:5432
-        run: 
-          bin/rails db:migrate db:test:prepare test test:system
-
-      - name: Keep screenshots from failed system tests
-        uses: actions/upload-artifact@v4
-        if: failure()
-        with:
-          name: screenshots
-          path: ${{ github.workspace }}/tmp/screenshots
-          if-no-files-found: ignore
-
-YAML
-  )
 end
+puts "Created Stimulus controller: #{javascript_dir}/flash_controller.js"
+
+# Ensure the views shared directory exists
+view_share_dir = 'app/views/shared'
+FileUtils.mkdir_p(view_share_dir) unless Dir.exist?(view_share_dir)
+puts "Created directory: #{view_share_dir}"
+
+# Create flash alert partial file
+inside(view_share_dir) do
+  write_file('_flash_notice.html.erb', <<-CODE
+    <% if notice %>
+      <div class="flash flash-notice mt-14 text-center w-full" data-controller="flash" data-action="animationend->flash#remove">
+        <%= notice %>
+      </div>
+    <% end %>
+    
+    <% if alert %>
+      <div class="flash flash-alert mt-14 text-center w-full" data-controller="flash" data-action="animationend->flash#remove">
+        <%= alert %>
+      </div>
+    <% end %>
+  CODE
+end
+puts "Created flash partial: #{view_share_dir}/_flash_notice.html.erb"
+
+# Ensure the css directory exists
+style_dir = 'app/assets/stylesheets'
+FileUtils.mkdir_p(style_dir) unless Dir.exist?(style_dir)
+puts "Created directory: #{style_dir}"
+
+# Path to css file
+file_path = File.join(style_dir, "application.css")
+
+# Classes to append
+classes_to_append = <<-CODE
+ .flash {
+  padding: 15px;
+  margin-bottom: 10px;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  opacity: 1;
+  transition: opacity 1s ease-out;
+}
+
+.flash-notice {
+  color: #D4E157;
+  background-color: #334155;
+  border-color: #d6e9c6;
+}
+
+.flash-alert {
+  color: #fc5a58;
+  background-color: #334155;
+  border-color: #ebccd1;
+}
+CODE
+
+# Append Classes to css file
+append_to_file(file_path, classes_to_append)
